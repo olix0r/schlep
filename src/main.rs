da326719cf8bb494447876b9a30ac6621e7f3d7c
Oliver Gould <ver@buoyant.io>
2024-06-19 20:34:39 +0000

    chore: cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use anyhow::Result;
 use bytes::Bytes;
 use clap::Parser;
 use http_body_util::Empty;
@@ -10,7 +11,7 @@ use std::{
 use tokio::time;
 
 #[tokio::main]
-async fn main() -> anyhow::Result<()> {
+async fn main() -> Result<()> {
     let Args {
         command,
         log_format,
@@ -22,93 +23,13 @@ async fn main() -> anyhow::Result<()> {
     let (shutdown, _handle) = kubert::shutdown::sigint_or_sigterm()?;
 
     match command {
-        Cmd::Server(ServerArgs {
-            port,
-            max_concurrent_streams,
-        }) => {
-            let mut server = hyper::server::conn::http2::Builder::new(TokioExecutor::new());
-            server.max_concurrent_streams(max_concurrent_streams);
-
-            tracing::info!(?port);
-            let listener = tokio::net::TcpListener::bind(
-                &[
-                    SocketAddr::new(Ipv4Addr::UNSPECIFIED.into(), port),
-                    SocketAddr::new(Ipv6Addr::UNSPECIFIED.into(), port),
-                ][..],
-            )
-            .await?;
-
-            tokio::pin! {
-                let signaled = shutdown.signaled();
+        Cmd::Server(args) => tokio::select! {
+            res = run_server(args) => res,
+            _ = shutdown.signaled() => {
+                tracing::info!("Shutting down");
+                Ok(())
             }
-            loop {
-                let (io, _addr) = tokio::select! {
-                    res = listener.accept() => res?,
-                    _ = &mut signaled => {
-                        tracing::info!("Shutting down");
-                        break
-                    }
-                };
-
-                let server = server.clone();
-                tokio::spawn(server.serve_connection(
-                    hyper_util::rt::TokioIo::new(io),
-                    hyper::service::service_fn(|req| async move {
-                        let start = time::Instant::now();
-
-                        let mut status = 204;
-                        let mut max_sleep = 0.0;
-                        let mut fail_rate = 0.0;
-
-                        if let Some(q) = req.uri().query() {
-                            for pair in q.split('&') {
-                                if let Some((key, val)) = pair.split_once('=') {
-                                    if key.eq_ignore_ascii_case("fail-rate") {
-                                        if let Ok(f) = val.parse() {
-                                            if f > 0.0 {
-                                                fail_rate = f;
-                                            }
-                                        }
-                                    } else if key.eq_ignore_ascii_case("max-sleep") {
-                                        if let Ok(s) = val.parse() {
-                                            if s > 0.0 {
-                                                max_sleep = s;
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                        if max_sleep > 0.0 {
-                            let sleep = rand::thread_rng().gen_range(0.0..=max_sleep);
-                            tracing::debug!(?sleep);
-                            time::sleep(time::Duration::from_secs_f64(sleep)).await;
-                        }
-
-                        if fail_rate > 0.0 {
-                            if rand::thread_rng().gen::<f64>() < fail_rate {
-                                status = 500;
-                            }
-                        }
-                        tracing::info!(
-                            status,
-                            fail_rate,
-                            elapsed = start.elapsed().as_secs_f64(),
-                            max_sleep
-                        );
-
-                        Ok::<_, Infallible>(
-                            hyper::Response::builder()
-                                .status(status)
-                                .body(Empty::<Bytes>::default())
-                                .unwrap(),
-                        )
-                    }),
-                ));
-            }
-
-            Ok(())
-        }
+        },
 
         Cmd::Client(ClientArgs {
             address,
@@ -116,84 +37,20 @@ async fn main() -> anyhow::Result<()> {
             max_sleep,
             fail_rate,
         }) => {
-            if !(rate > 0.0) {
+            if rate.partial_cmp(&0.0) != Some(std::cmp::Ordering::Greater) {
                 anyhow::bail!("--rate must be greater than zero");
             }
 
             let uri = format!("http://{address}/?max-sleep={max_sleep}&fail-rate={fail_rate}")
                 .parse::<hyper::Uri>()?;
 
-            tokio::pin! {
-                let signaled = shutdown.signaled();
+            tokio::select! {
+                res = run_client(&address, uri, rate) => res,
+                _ = shutdown.signaled() => {
+                    tracing::info!("Shutting down");
+                    Ok(())
+                }
             }
-
-            // Connect to the server.
-            let (mut client, conn) = loop {
-                match tokio::net::TcpStream::connect(&address).await {
-                    Ok(conn) => {
-                        match hyper::client::conn::http2::Builder::new(TokioExecutor::new())
-                            .handshake::<_, Empty<Bytes>>(TokioIo::new(conn))
-                            .await
-                        {
-                            Ok(conn) => break conn,
-                            Err(error) => {
-                                tracing::warn!(%error, address, "Failed to connect");
-                            }
-                        }
-                    }
-                    Err(error) => {
-                        tracing::warn!(%error, address, "Failed to connect");
-                    }
-                };
-
-                time::sleep(time::Duration::from_secs(1)).await;
-            };
-            tokio::spawn(async move {
-                if let Err(error) = conn.await {
-                    tracing::error!(%error, "Connection error");
-                }
-            });
-
-            let mut timer = time::interval(time::Duration::from_secs_f64(1.0 / rate));
-            timer.set_missed_tick_behavior(time::MissedTickBehavior::Delay);
-
-            loop {
-                tokio::select! {
-                    _ = timer.tick() => {}
-                    _ = &mut signaled => {
-                        tracing::info!("Shutting down");
-                        break;
-                    }
-                }
-
-                let req = hyper::Request::builder()
-                    .uri(uri.clone())
-                    .body(Empty::<Bytes>::default())
-                    .unwrap();
-                let start = time::Instant::now();
-
-                tokio::select! {
-                    res = client.ready() => res?,
-                    _ = &mut signaled => {
-                        tracing::info!("Shutting down");
-                        break;
-                    }
-                }
-
-                let call = client.send_request(req);
-                tokio::spawn(async move {
-                    match call.await {
-                        Ok(res) => {
-                            tracing::info!(status = ?res.status(), elapsed = start.elapsed().as_secs_f64());
-                        }
-                        Err(error) => {
-                            tracing::warn!(%error, "Request failed");
-                        }
-                    }
-                });
-            }
-
-            Ok(())
         }
     }
 }
@@ -238,4 +95,139 @@ struct ServerArgs {
 
     #[arg(long)]
     max_concurrent_streams: Option<u32>,
+}
+
+async fn run_server(
+    ServerArgs {
+        port,
+        max_concurrent_streams,
+    }: ServerArgs,
+) -> Result<()> {
+    let mut server = hyper::server::conn::http2::Builder::new(TokioExecutor::new());
+    server.max_concurrent_streams(max_concurrent_streams);
+
+    tracing::info!(?port);
+    let listener = tokio::net::TcpListener::bind(
+        &[
+            SocketAddr::new(Ipv4Addr::UNSPECIFIED.into(), port),
+            SocketAddr::new(Ipv6Addr::UNSPECIFIED.into(), port),
+        ][..],
+    )
+    .await?;
+
+    loop {
+        let (io, _addr) = listener.accept().await?;
+
+        let server = server.clone();
+        tokio::spawn(server.serve_connection(
+            hyper_util::rt::TokioIo::new(io),
+            hyper::service::service_fn(|req| async move {
+                let start = time::Instant::now();
+
+                let mut status = 204;
+                let mut max_sleep = 0.0;
+                let mut fail_rate = 0.0;
+
+                if let Some(q) = req.uri().query() {
+                    for pair in q.split('&') {
+                        if let Some((key, val)) = pair.split_once('=') {
+                            if key.eq_ignore_ascii_case("fail-rate") {
+                                if let Ok(f) = val.parse() {
+                                    if f > 0.0 {
+                                        fail_rate = f;
+                                    }
+                                }
+                            } else if key.eq_ignore_ascii_case("max-sleep") {
+                                if let Ok(s) = val.parse() {
+                                    if s > 0.0 {
+                                        max_sleep = s;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                if max_sleep > 0.0 {
+                    let sleep = rand::thread_rng().gen_range(0.0..=max_sleep);
+                    tracing::debug!(?sleep);
+                    time::sleep(time::Duration::from_secs_f64(sleep)).await;
+                }
+
+                if fail_rate > 0.0 && rand::thread_rng().gen::<f64>() < fail_rate {
+                    status = 500;
+                }
+                tracing::info!(
+                    status,
+                    fail_rate,
+                    elapsed = start.elapsed().as_secs_f64(),
+                    max_sleep
+                );
+
+                Ok::<_, Infallible>(
+                    hyper::Response::builder()
+                        .status(status)
+                        .body(Empty::<Bytes>::default())
+                        .unwrap(),
+                )
+            }),
+        ));
+    }
+}
+
+async fn run_client(address: &str, uri: hyper::Uri, rate: f64) -> Result<()> {
+    // Connect to the server.
+    let (mut client, conn) = loop {
+        match tokio::net::TcpStream::connect(address).await {
+            Ok(conn) => {
+                match hyper::client::conn::http2::Builder::new(TokioExecutor::new())
+                    .handshake::<_, Empty<Bytes>>(TokioIo::new(conn))
+                    .await
+                {
+                    Ok(conn) => break conn,
+                    Err(error) => {
+                        tracing::warn!(%error, address, "Failed to connect");
+                    }
+                }
+            }
+            Err(error) => {
+                tracing::warn!(%error, address, "Failed to connect");
+            }
+        };
+
+        time::sleep(time::Duration::from_secs(1)).await;
+    };
+    tokio::spawn(async move {
+        if let Err(error) = conn.await {
+            tracing::error!(%error, "Connection error");
+        }
+    });
+
+    let mut timer = time::interval(time::Duration::from_secs_f64(1.0 / rate));
+    timer.set_missed_tick_behavior(time::MissedTickBehavior::Delay);
+
+    loop {
+        timer.tick().await;
+
+        let req = hyper::Request::builder()
+            .uri(uri.clone())
+            .body(Empty::<Bytes>::default())
+            .unwrap();
+        let start = time::Instant::now();
+
+        tokio::select! {
+            res = client.ready() => res?
+        }
+
+        let call = client.send_request(req);
+        tokio::spawn(async move {
+            match call.await {
+                Ok(res) => {
+                    tracing::info!(status = ?res.status(), elapsed = start.elapsed().as_secs_f64());
+                }
+                Err(error) => {
+                    tracing::warn!(%error, "Request failed");
+                }
+            }
+        });
+    }
 }
